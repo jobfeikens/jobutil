8fc11df89dd60eae95c9ae72413ddd274a318437
Job Feikens <job@vv32.nl>
2020-01-20 15:56:08 +0100

    Renamed from() to compose()

--- src/main/java/nl/vv32/jobutil/lang/Subscription.java
+++ src/main/java/nl/vv32/jobutil/lang/Subscription.java
@@ -5,11 +5,11 @@ public interface Subscription extends Disposable {
     default Subscription add(Subscription... subscriptions) {
         return () -> {
             this.dispose();
-            from(subscriptions).dispose();
+            compose(subscriptions).dispose();
         };
     }
 
-    static Subscription from(Subscription... subscriptions) {
+    static Subscription compose(Subscription... subscriptions) {
         return () -> {
             for (Subscription subscription : subscriptions) {
                 subscription.dispose();
